ba3a571622cf37107ef2348b78552b74f260f845
boxcontrol <info@boxcontrol.net>
2016-01-22 11:09:22 +0100

    Adding standard and custom navigation toolbar

---
+++ addingSaveButton.py
@@ -0,0 +1,144 @@
+import sys
+import random
+import matplotlib
+matplotlib.use("Qt5Agg")
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, QPushButton
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+
+
+# customize navigation toolbar
+class NavToolbar(NavigationToolbar):
+    toolitems = [('Save', 'Save the figure', 'filesave', 'save_figure')]
+
+
+class MyMplCanvas(FigureCanvas):
+    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+    def __init__(self, parent=None, width=5, height=4, dpi=100, title='title'):
+        self.title = title
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        fig.suptitle(title)
+
+        # We want the axes cleared every time plot() is called
+        self.axes.hold(False)
+
+        self.compute_initial_figure()
+
+
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                QSizePolicy.Expanding,
+                QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+    def compute_initial_figure(self):
+        pass
+
+
+class MyStaticMplCanvas(MyMplCanvas):
+    """Simple canvas with a sine plot."""
+    def compute_initial_figure(self):
+        t = arange(0.0, 3.0, 0.01)
+        s = sin(2*pi*t)
+        self.axes.plot(t, s)
+        self.axes.set_ylabel('label1')
+        self.axes.set_xlabel('label')
+        self.axes.grid(True)
+        #self.axes.set_ylim(0, 0.5)
+
+
+class MyDynamicMplCanvas(MyMplCanvas):
+    """A canvas that updates itself every second with a new plot."""
+    def __init__(self, *args, **kwargs):
+        MyMplCanvas.__init__(self, *args, **kwargs)
+        timer = QtCore.QTimer(self)
+        timer.timeout.connect(self.update_figure)
+        timer.start(1000)
+
+    def compute_initial_figure(self):
+        self.axes.plot([0, 1, 2, 3], [1, 2, 0, 4], 'r')
+        self.axes.set_ylabel('label1')
+        self.axes.set_xlabel('label')
+        self.axes.grid(True)
+
+    def update_figure(self):
+        # Build a list of 4 random integers between 0 and 10 (both inclusive)
+        l = [random.randint(0, 10) for i in range(4)]
+        self.axes.plot([0, 1, 2, 3], l, 'r')
+        self.axes.set_ylabel('label y din plot')
+        self.axes.set_xlabel('label x din plot')
+        self.axes.grid(True)
+        self.draw()
+
+
+class ApplicationWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.setWindowTitle("application main window")
+
+        self.file_menu = QMenu('&File', self)
+        self.file_menu.addAction('&Quit', self.fileQuit,
+                QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
+        self.menuBar().addMenu(self.file_menu)
+
+        self.help_menu = QMenu('&Help', self)
+        self.menuBar().addSeparator()
+        self.menuBar().addMenu(self.help_menu)
+
+        self.help_menu.addAction('&About', self.about)
+
+        self.main_widget = QWidget(self)
+
+        l = QVBoxLayout(self.main_widget)
+        sc = MyStaticMplCanvas(self.main_widget, width=5, height=4, dpi=100, title='Title 1')
+        dc = MyDynamicMplCanvas(self.main_widget, width=5, height=4, dpi=100, title='Title 2')
+        scntb = NavigationToolbar(sc, self.main_widget) # full toolbar
+        dcntb = NavToolbar(dc, self.main_widget) # only save button
+
+        l.addWidget(sc)
+        l.addWidget(scntb)
+        l.addWidget(dc)
+        l.addWidget(dcntb)
+
+        self.main_widget.setFocus()
+        self.setCentralWidget(self.main_widget)
+
+        self.statusBar().showMessage("All hail matplotlib!", 2000)
+
+
+    def fileQuit(self):
+        self.close()
+
+    def closeEvent(self, ce):
+        self.fileQuit()
+
+    def about(self):
+        QMessageBox.about(self, "About",
+  """embedding_in_qt5.py example
+  Copyright 2015 BoxControL
+
+  This program is a simple example of a Qt5 application embedding matplotlib
+  canvases. It is base on example from matplolib documentation, and initially was
+  developed from Florent Rougon and Darren Dale.
+
+  http://matplotlib.org/examples/user_interfaces/embedding_in_qt4.html
+
+  It may be used and modified with no restriction; raw copies as well as
+  modified versions may be distributed without limitation.""")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    aw = ApplicationWindow()
+    aw.setWindowTitle("PyQt5 Matplot Example")
+    aw.show()
+    #sys.exit(qApp.exec_())
+    app.exec_()
